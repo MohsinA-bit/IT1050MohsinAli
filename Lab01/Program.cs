d57572abf191e007264fffbe3f916b6e6b279214
MohsinA-bit <56942181+MohsinA-bit@users.noreply.github.com>
2019-10-23 19:42:31 -0400

    Add files via upload

---
+++ Lab01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Week_1_Mohsin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1, str2;
+
+            str1 = "Hello World";
+
+            str2 = "Mohsin Ali";
+
+
+            Console.WriteLine(str1 + "from" + str2);
+
+
+
+
+
+
+
+
+
+           
+        }
+    }
+}
